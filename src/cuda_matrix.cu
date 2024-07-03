7f65990f416b59d21b97a34445c4cb1fdcf4fb08
jbrhm <jrabraham1904@gmail.com>
2024-07-03 10:46:12 -0400

    changed to use both dimensions

--- src/cuda_matrix.cu
+++ src/cuda_matrix.cu
@@ -1,12 +1,11 @@
 #include "cuda_matrix.cuh"
 
 __global__ void rowColProduct(double* dataA, double* dataB, double* dataC, unsigned int N){
-	int id = threadIdx.x + blockIdx.x * blockDim.x;
+	unsigned int col = threadIdx.x + blockIdx.x * blockDim.x;
 
-	if(id < N * N){
-		int row = id / N;
-		int col = id % N;
+	unsigned int row = threadIdx.x + blockIdx.x * blockDim.x;
 
+	if(col < N && row < N){
 		double sum = 0;
 
 		for(unsigned int i = 0; i < N; ++i){
@@ -28,11 +27,13 @@ void cudaMatrix::syncHost(double* hostData){
 
 void cudaMatrix::multiply(cudaMatrix &matA, cudaMatrix &matB, cudaMatrix &matC){
 
-	constexpr unsigned int THREADS = 32;
+	constexpr unsigned int BLOCK_DIM = 32;
+	constexpr dim3 blockDimension{BLOCK_DIM, BLOCK_DIM};
+	dim3 gridDimension{static_cast<unsigned int>(std::ceil(static_cast<double>(matA.mN)/BLOCK_DIM)), static_cast<unsigned int>(std::ceil(static_cast<double>(matA.mN)/BLOCK_DIM))};
 
 	if(matA.mN != matB.mN || matA.mN != matC.mN) throw std::runtime_error("Matrices are not the same size!");
 
-	rowColProduct<<<std::ceil((matA.mN * matA.mN)/static_cast<double>(THREADS)), THREADS>>>(matA.mData, matB.mData, matC.mData, matA.mN);
+	rowColProduct<<<gridDimension, blockDimension>>>(matA.mData, matB.mData, matC.mData, matA.mN);
 }
 
 cudaMatrix::~cudaMatrix(){
