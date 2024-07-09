1b508f7722236ff98f879eb57d721782f14ed8bb
jbrhm <jrabraham1904@gmail.com>
2024-07-09 15:05:50 -0400

    Building correct

---
+++ build.sh
@@ -0,0 +1,8 @@
+sudo -v
+cmake . -B build
+cmake --build build
+cmake --install build
+cd package/
+python3 -m build
+cd dist
+sudo pip install cupybara_jbrhm-1.0.0-py3-none-any.whl
---
+++ package/README.md
@@ -0,0 +1,1 @@
+This is a python package for the cupybara matrix multiplication library
